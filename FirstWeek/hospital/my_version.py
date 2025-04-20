60b00a938f3fdebd7cc14d9a035cea486c6c1fc8
IsabelaCaldeira <isabela.ribeiro@outlook.com>
2025-04-20 10:21:49 +0200

    adding comments

--- FirstWeek/hospital/my_version.py
+++ FirstWeek/hospital/my_version.py
@@ -4,10 +4,8 @@ from stanfordkarel import *
 
 def main():
     """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
+    Karel builds a hospital (two columns of three rows of beepers)
+    everytime it finds a beeper in the first row. 
     """
     while(front_is_clear()):
         if(beepers_present()):
