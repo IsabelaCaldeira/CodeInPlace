954e29f967d2718879decf268386d5922c5e194e
IsabelaCaldeira <isabela.ribeiro@outlook.com>
2025-05-29 09:07:22 +0200

    adding challenges

---
+++ SixthWeek/challenges/memory_game.py
@@ -0,0 +1,17 @@
+import random
+
+NUM_PAIRS = 3
+
+def main():
+    """
+    You should write your code here. Make sure to delete 
+    the 'pass' line before starting to write your own code.
+    """
+    pass
+    
+def clear_terminal():
+    for i in range(20):
+      print('\n')
+
+if __name__ == '__main__':
+    main()
